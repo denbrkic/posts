a51796e3397540fc94fc92efaa58c5ee5d8d10b7
Denis Brkic <denis.brkic.main@gmail.com>
2022-02-11 19:52:50 +0100

    [ADD] add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.scss';
-
-import Button from 'react-bootstrap/Button';
+import { Switch, Route } from 'react-router-dom';
+import Home from './views/Home/Home';
+import Posts from './views/Posts/Posts';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>{' '}
+      <Switch>
+				<Route path="/" exact component={Home} />
+				<Route path="/posts" exact component={Posts} />
+			</Switch>
     </div>
   );
 }
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const Home = () => {
+    return (
+        <>
+            <h1>Welcome to our forum</h1>
+            <Button variant="outline-primary">Check out some posts</Button>
+        </>        
+    );
+}
+
+export default Home;
