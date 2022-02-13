8d17972e5f1ed55c9c3f767c3927fbd9c9a1f0b9
Denis Brkic <denis.brkic.main@gmail.com>
2022-02-13 13:57:36 +0100

    [REFACTOR] make a constant for post comments

--- src/api/api.ts
+++ src/api/api.ts
@@ -13,7 +13,7 @@ export const fetchUsers = async () => {
     return await axios(`${constants.BASE_URL}${constants.USERS}`);
 }
 
-export const fetchPost = async (postId: number | string) => {
+export const fetchPost = async (postId: string) => {
     if (!postId) return;
     return await axios(`${constants.BASE_URL}${constants.POSTS}/${postId}`);
 }
@@ -23,7 +23,7 @@ export const fetchUser = async (userId: number | string) => {
     return await axios(`${constants.BASE_URL}${constants.USERS}/${userId}`);
 }
 
-export const fetchPostComments = async (postId: number | string) => {
+export const fetchPostComments = async (postId: string) => {
     if (!postId) return;
-    return await axios(`${constants.BASE_URL}${constants.POSTS}/${postId}${constants.COMMENTS}`);
+    return await axios(`${constants.BASE_URL}${constants.POST_COMMENTS}`.replace(':postId', postId));
 }
--- src/constants/api.ts
+++ src/constants/api.ts
@@ -2,3 +2,4 @@ export const BASE_URL = 'https://jsonplaceholder.typicode.com';
 export const POSTS = '/posts';
 export const COMMENTS = '/comments';
 export const USERS = '/users';
+export const POST_COMMENTS = '/posts/:postId/comments';
